cb658fc0da8aea3fefd8a89b931097d2d86155a0
eric <eric.rian.silva.carlos@gmail.com>
2023-04-19 17:49:02 -0300

    Aula_php

--- src/public/index.php
+++ src/public/index.php
@@ -1,3 +1,39 @@
 <?php
+ $primeiro_nome = "Maria";
+ echo $primeiro_nome .'<br />';
+//phpinfo();
 
-phpinfo();
+$time = array("Maria","José","Ana");
+
+echo $time[2];
+
+echo '<br>';
+
+print_r($time);
+
+echo 'x: [' .TRUE . ']<br />';
+echo 'x: [' .FALSE . ']<br />';
+
+$Nome = "Maria";
+$idade = [];
+
+echo "x: (.$primeiro_nome.)";
+
+if (condition) {
+    # code...
+} else {
+    # code...
+}
+
+switch ($Variable_menu) {
+    case 'value':
+        echo ""
+        break;
+    
+    default:
+        # code...
+        break;
+}
+
+
+?>
